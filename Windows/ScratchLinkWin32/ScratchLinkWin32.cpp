8c151d450102a12dee614752fc9b68ec0869f2c7
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-11-29 11:24:30 -0800

    Adjust ScratchLinkWin32 for "Any CPU" compatibility

--- Windows/ScratchLinkWin32/ScratchLinkWin32.cpp
+++ Windows/ScratchLinkWin32/ScratchLinkWin32.cpp
@@ -2,3 +2,9 @@
 
 #include "ScratchLinkWin32.h"
 
+#pragma warning(push)
+#pragma warning(disable:4483)
+// Ensure compatibility with "Any CPU": we need a module static constructor
+void __clrcall __identifier(".cctor")() {
+}
+#pragma warning(pop)
